23af2929693d87e0d0e464845eceae77045a9ccb
Sahil Khandare <sahil.khandare2021@gmail.com>
2026-06-06 16:05:53 +0530

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE department(...);
+
+CREATE TABLE student(...);
+
+CREATE TABLE faculty(...);
+
+CREATE TABLE course(...);
+
+CREATE TABLE enrollment(...);
+
+CREATE TABLE classroom(...);
+
+CREATE TABLE hostel(...);
+
+CREATE TABLE result(...);
